d2b60236ab86b5bdc14f287e325eef126167ff4f
Charles Pelton <charliejp0311@gmail.com>
2020-01-01 21:42:19 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -10,16 +10,26 @@ def first_challenge
          :age => "54"
        },
        :hero => {
-         :name => "Romeo"
+         :name => "Romeo",
+         :age => "15",
+         :status => "alive"
        },
-       :hero_friends => {
-         
-       }
+       :hero_friends => {}
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
+       :patriarch => {
+         :name => "Lord Capulet",
+         :age => "50"
+       },
+       :matriarch => {
+         :name => "Lady Capulet",
+         :age => "51"
+       },
+       :heroine => {
+         :name => "Juliet",
+         :age => "15",
+         :status => "alive"
+       },
        :heroine_friends => {}
      }
    }
